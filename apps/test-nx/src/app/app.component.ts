89984c929d9db0a0801985a5efb3aa99d758ca62
Carles Company Soler <carles@company.cat>
2023-07-28 15:37:22 +0000

    Add ui library

--- apps/test-nx/src/app/app.component.ts
+++ apps/test-nx/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { UiModule } from '@test-nx/ui';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, UiModule],
   selector: 'test-nx-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'test-nx';
+  
 }
