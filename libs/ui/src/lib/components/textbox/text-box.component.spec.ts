765706497ae1e7f400c0de71d8e265225e67497f
Carles Company Soler <carlescs@gmail.com>
2023-09-03 19:35:47 +0200

    Fix tests for text-box.component.

--- libs/ui/src/lib/components/textbox/text-box.component.spec.ts
+++ libs/ui/src/lib/components/textbox/text-box.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TextBoxComponent } from './text-box.component';
+import { FormsModule } from '@angular/forms';
 
 describe('TextBoxComponent', () => {
    let component: TextBoxComponent;
@@ -7,6 +8,7 @@ describe('TextBoxComponent', () => {
 
    beforeEach(async () => {
       await TestBed.configureTestingModule({
+         imports: [FormsModule],
          declarations: [TextBoxComponent],
       }).compileComponents();
 
